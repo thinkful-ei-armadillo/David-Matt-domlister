7e2f00640cc5d1e1962d6e0e42f59d5a56f0580d
David Haugen <haugen.dj@gmail.com>
2019-01-17 12:30:19 -0600

    add fizzbuzz challenge

--- fizz/index.js
+++ fizz/index.js
@@ -3,22 +3,27 @@
 
 $(function fizz(){
   $('#number-chooser').submit(function(event){
-    event.preventDefault();
+    event.preventDefault()
 
     const input = $(this).find('input[name="number-choice"]').val();
     
     for (let i=1; i <= input; i++) {
+      console.log(`i is ${i}`);
       if (i % 3 === 0 && i % 5 === 0) {
-        
-      }
-      else if (i % 3 === 0){
-        
-      }
+        $('.js-results').append(`<div class="fizz-buzz-item fizz">
+        <span>fizz</span>
+      </div>`);
+      } else if (i % 3 === 0){
+        $('.js-results').append(`<div class="fizz-buzz-item buzz">
+        <span>buzz</span>
+      </div>`);}
       else if (i % 5 === 0) {
-        
+        $('.js-results').append(`<div class="fizz-buzz-item fizzbuzz">
+        <span>fizzbuzz</span>
+      </div>`);}
+      else {$('.js-results').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
       }
-      else {}
     }
-
   });
+  
 });
